25fa5c0b43c75969f83229d251a8c3fcbb77eda6
torrar <art.torres.alv@gmail.com>
2019-01-21 18:50:18 -0600

    Adding border templates

---
+++ src/QApp/Util/BorderTemplates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QApp.Util
+{
+    public class BorderTemplates
+    {
+        public static char[] DOTTED = new char[] {
+            '·', '·', '·',
+            '·', ' ', '·',
+            '·', '·', '·'
+        };
+
+        public static char[] STARS = new char[] {
+            '*', '*', '*',
+            '*', ' ', '*',
+            '*', '*', '*'
+        };
+
+        public static char[] RIDGE = new char[] {
+            '╔', '═', '╗',
+            '║', ' ', '║',
+            '╚', '═', '╝'
+        };
+
+        public static char[] SOLID = new char[] {
+            '█', '█', '█',
+            '█', '█', '█',
+            '█', '█', '█'
+        };
+    }
+}
