dad83dc7a47bcf9ba5cd16e1c79d0a0d1e19e71a
torrar <art.torres.alv@gmail.com>
2019-02-05 07:27:31 -0600

    Adding widget examples

--- test/TWidgets.DemoApp/Program.cs
+++ test/TWidgets.DemoApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using TWidgets;
 using TWidgets.Util;
 using TWidgets.Widgets;
@@ -8,6 +9,14 @@ namespace DemoApp
     class Program
     {
         static void Main(string[] args)
+        {
+            ProgressBarSample();
+
+            Console.WriteLine("Text with System Color");
+            Console.ReadKey();
+        }
+
+        static void MessageSample()
         {
             var msg = new Message("txt")
             {
@@ -19,7 +28,10 @@ namespace DemoApp
             WidgetPlayer.Mount(msg);
 
             Console.WriteLine("--END--");
+        }
 
+        static void MarqueeSample()
+        {
             var marquee = new Marquee("mar");
             marquee.Items = new string[] {
                 "Hello World",
@@ -33,7 +45,10 @@ namespace DemoApp
             WidgetPlayer.Mount(marquee);
 
             Console.WriteLine("--END--");
+        }
 
+        static void SeparatorSample()
+        {
             var separator = new Separator("sep");
             separator.Margin.All = 1;
             separator.ForegroundColor = WidgetColor.Yellow;
@@ -41,7 +56,10 @@ namespace DemoApp
             WidgetPlayer.Mount(separator);
 
             Console.WriteLine("--END--");
+        }
 
+        static void BulletSample()
+        {
             var list = new BulletList("lst");
             list.Margin.All = 1;
             list.BackgroundColor = WidgetColor.DarkBlue;
@@ -55,10 +73,49 @@ namespace DemoApp
             WidgetPlayer.Mount(list);
 
             Console.WriteLine("--END--");
+        }
 
-            Console.WriteLine("Text with System Color");
+        static void TextInputSample()
+        {
+            var tInput = new TextInput("txi");
 
-            Console.ReadKey();
+            WidgetPlayer.Mount(tInput);
+
+            Console.WriteLine("--END--");
+        }
+
+        static void ProgressBarSample()
+        {
+            var progress = new ProgressBar("pb");
+            progress.Width = 0;
+            progress.Margin.All = 1;
+            progress.Template = ProgressBarTemplates.ARROW;
+
+            WidgetPlayer.Mount(progress);
+
+            for (int i = 0; i < 10; i++)
+            {
+                progress.PerformStep();
+                Thread.Sleep(200);
+            }
+
+            Console.WriteLine("--END--");
+        }
+
+        static void ProgressCharSample()
+        {
+            var progress = new ProgressChar("pc");
+            progress.Text = "Start Message";
+
+            WidgetPlayer.Mount(progress);
+
+            for (int i = 0; i < 5;)
+            {
+                progress.Text = $"Message {++i:00}";
+                Thread.Sleep(500);
+            }
+
+            Console.WriteLine("--END--");
         }
     }
 }
