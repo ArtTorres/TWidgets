3ea33e55b758f9047adaf5b064c1643d82db9e23
torrar <art.torres.alv@gmail.com>
2019-02-05 07:14:46 -0600

    Adding ProgressBar widget

---
+++ src/TWidgets/Util/ProgressBarTemplates.cs
@@ -0,0 +1,36 @@
+﻿
+namespace TWidgets.Util
+{
+    public static class ProgressBarTemplates
+    {
+        // [■■■■■■■■■]
+        public static readonly char[] SIMPLE = new char[] {
+            '[', ' ', '■', ']'
+        };
+
+        // ¦█████████¦
+        public static readonly char[] SOLID = new char[] {
+            '¦', ' ', '█', '¦'
+        };
+
+        // [>>>>>>>>>]
+        public static readonly char[] ARROW = new char[] {
+            '[', ' ', '>', ']'
+        };
+
+        // [\\\\\\\\\]
+        public static readonly char[] INVERTED_BACKSLASH = new char[] {
+            '[', ' ', '\\', ']'
+        };
+
+        // [/////////]
+        public static readonly char[] BACKSLASH = new char[] {
+            '[', ' ', '/', ']'
+        };
+
+        // ¦|||||||||¦
+        public static readonly char[] BARS = new char[] {
+            '[', ' ', '/', ']'
+        };
+    }
+}
--- src/TWidgets/Widgets/ProgressBar.cs
+++ src/TWidgets/Widgets/ProgressBar.cs
@@ -1,24 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using TWidgets.Core;
 using TWidgets.Core.Drawing;
+using TWidgets.Util;
 
 namespace TWidgets.Widgets
 {
     public class ProgressBar : BoxWidget
     {
-        public double Step { get; set; } = 1.0d;
-        public double Max { get; set; } = 100.0d;
-        public double Min { get; set; } = 0.0d;
+        private double _maximum;
+        public double Maximum
+        {
+            get
+            {
+                return _maximum;
+            }
+            set
+            {
+                if (value < Minimum) Minimum = value;
+                if (value < Value) Value = value;
 
-        public double Progress { get; private set; }
+                _maximum = value;
+            }
+        }
 
-        private const int BAR_START = 0;
-        private const int BAR_BACKGROUND = 1;
-        private const int BAR_FILLED = 2;
-        private const int BAR_END = 3;
+        private double _minimum;
+        public double Minimum
+        {
+            get
+            {
+                return _minimum;
+            }
+            set
+            {
+                if (value > Maximum) Maximum = value;
+                if (value > Value) Value = value;
 
+                _minimum = value;
+            }
+        }
+
+        private double _value;
+        public double Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (value > Maximum)
+                    _value = Maximum;
+                else if (value < Minimum)
+                    _value = Minimum;
+                else
+                    _value = value;
+
+                this.OnStateChanged();
+            }
+        }
+
+        public double Step { get; set; } = 10.0d;
+
+        public const int BAR_START = 0;
+        public const int BAR_BACKGROUND = 1;
+        public const int BAR_FILLED = 2;
+        public const int BAR_END = 3;
         public const int TEMPLATE_SIZE = 4;
 
         private char[] _template;
@@ -39,12 +85,38 @@ namespace TWidgets.Widgets
 
         public ProgressBar(string id) : base(id)
         {
-            _template = new char[] { '[', ' ', '█', ']' };
+            _maximum = 100.0d;
+            _minimum = 0.0d;
+
+            _template = ProgressBarTemplates.SIMPLE;
+            this.Position = Position.Fixed;
         }
 
         public override void Draw(Graphics g)
         {
-            throw new NotImplementedException();
+            g.Draw(new Text(
+                $"{Template[BAR_START]}{ComposeBar(g.Canvas.Width)}{Template[BAR_END]}",
+                this.Margin
+                )
+            );
+        }
+
+        public void PerformStep()
+        {
+            this.Value += this.Step;
+        }
+
+        private string ComposeBar(int canvasWidth)
+        {
+            double width = (this.Width == 0 ? canvasWidth : this.Width) - 2 - this.Margin.Left - this.Margin.Right;
+
+            int filled = (int)(Value * width / Maximum);
+            int background = (int)(width - filled);
+
+            return string.Concat(
+                new string(Template[BAR_FILLED], filled),
+                new string(Template[BAR_BACKGROUND], background)
+            );
         }
     }
 }
