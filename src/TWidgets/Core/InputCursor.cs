94ce3d3ef66537e63494db8a25f11d4a1e6dd9a1
torrar <art.torres.alv@gmail.com>
2019-02-05 07:17:52 -0600

    Adding InputEngine base code

---
+++ src/TWidgets/Core/InputCursor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TWidgets.Core
+{
+    public class InputCursor
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+    }
+}
---
+++ src/TWidgets/Core/InputEngine.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TWidgets.Core
+{
+    internal sealed class InputEngine
+    {
+        #region Instance
+
+        private static readonly Lazy<InputEngine> instance = new Lazy<InputEngine>(() => new InputEngine());
+
+        public static InputEngine Instance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        #endregion
+
+        public event EventHandler<EventArgs> BeforeRender;
+        public void OnBeforeRender()
+        {
+            this.BeforeRender?.Invoke(this, new EventArgs());
+        }
+
+        public InputCursor SystemCursor { get; private set; }
+
+        public InputCursor Cursor
+        {
+            get
+            {
+                return new InputCursor()
+                {
+                    X = Console.CursorLeft,
+                    Y = Console.CursorTop
+                };
+            }
+
+            set
+            {
+                Console.SetCursorPosition(value.X, value.Y);
+            }
+        }
+
+        private InputEngine()
+        {
+            SystemCursor = new InputCursor();
+        }
+
+        public void SaveSystemCursor()
+        {
+            SystemCursor.X = Console.CursorLeft;
+            SystemCursor.Y = Console.CursorTop;
+        }
+
+        public void LoadSystemCursor()
+        {
+            Console.SetCursorPosition(
+                SystemCursor.X,
+                SystemCursor.Y
+            );
+        }
+
+        public char Read()
+        {
+            return (char)Console.Read();
+        }
+
+        public char ReadKey()
+        {
+            return Console.ReadKey().KeyChar;
+        }
+
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
