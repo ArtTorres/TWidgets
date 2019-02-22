5e521dd3fc2222faa46188884da5e69a014a818f
torrar <art.torres.alv@gmail.com>
2019-02-22 01:06:55 -0600

    Adding tests for Canvas, Graphics and Widgets related with the align behavior

---
+++ test/TWidgets.Test/CanvasTest.cs
@@ -0,0 +1,83 @@
+﻿using TWidgets.Core.Drawing;
+using Xunit;
+
+namespace TWidgets.Test
+{
+    public class CanvasTest
+    {
+        [Fact]
+        public void DrawLine()
+        {
+            var canvas = new Canvas(20, 10, '#');
+
+            canvas.DrawLine("Hello World");
+
+            Assert.Equal("Hello World#########", canvas.Map[0]);
+            Assert.Equal(1, canvas.Rows);
+        }
+
+        [Fact]
+        public void DrawInPosition()
+        {
+            var canvas = new Canvas(20, 10, '#');
+
+            canvas.Draw("Hello World", 3, 1);
+            canvas.RowCursor = 2;
+
+            Assert.Equal("####################", canvas.Map[0]);
+            Assert.Equal("###Hello World######", canvas.Map[1]);
+            Assert.Equal(2, canvas.Rows);
+        }
+
+        [Fact]
+        public void CursorPositions()
+        {
+            var canvas = new Canvas(20, 10, '#');
+
+            canvas.ColumnCursor = 2;
+            canvas.RowCursor = 2;
+
+            canvas.Draw("Hello World");
+
+            canvas.RowCursor += 1;
+
+            Assert.Equal("####################", canvas.Map[0]);
+            Assert.Equal("####################", canvas.Map[1]);
+            Assert.Equal("##Hello World#######", canvas.Map[2]);
+            Assert.Equal(3, canvas.Rows);
+        }
+
+        [Fact]
+        public void ResetCanvas()
+        {
+            var canvas = new Canvas(20, 10);
+
+            canvas.DrawLine("Hello World");
+            canvas.DrawLine("Hello World");
+            canvas.DrawLine("Hello World");
+
+            Assert.Equal(3, canvas.RowCursor);
+            Assert.Equal(3, canvas.Rows);
+
+            canvas.Clear();
+
+            Assert.Equal(0, canvas.RowCursor);
+            Assert.Empty(canvas.Map);
+            Assert.Equal(0, canvas.Rows);
+        }
+
+        //[Fact]
+        //public void DrawWithAlign()
+        //{
+        //    var canvas = new Canvas(20, 10, '#');
+        //    canvas.DrawLine("Hello World", Align.Left);
+        //    canvas.DrawLine("Hello World", Align.Center);
+        //    canvas.DrawLine("Hello World", Align.Right);
+
+        //    Assert.Equal("Hello World#########", canvas.Map[0]);
+        //    Assert.Equal("#####Hello World####", canvas.Map[1]);
+        //    Assert.Equal("#########Hello World", canvas.Map[2]);
+        //    Assert.Equal(3, canvas.Rows);
+        //}
+    }
+}
--- test/TWidgets.Test/WidgetTests.cs
+++ test/TWidgets.Test/WidgetTests.cs
@@ -1,20 +1,206 @@
-using System;
+﻿using TWidgets.Core.Drawing;
+using TWidgets.Widgets;
 using Xunit;
 
-namespace QApp.Test
+namespace TWidgets.Test
 {
     public class WidgetTests
     {
         [Fact]
         public void ProgressChar()
         {
-
+            var canvas = new Canvas(20, 10, '#');
+            var graphics = new Graphics(canvas);
         }
 
         [Fact]
         public void ProgressBar()
         {
+            var canvas = new Canvas(20, 10, '#');
+            var graphics = new Graphics(canvas);
+        }
 
+        [Fact]
+        public void MessageTest()
+        {
+            var canvas = new Canvas(20, 10, '#');
+            var graphics = new Graphics(canvas);
+
+            var widget = new Message("demo");
+            widget.Text = "Hello World";
+            widget.Margin.Left = 1;
+            widget.Margin.Right = 1;
+
+            widget.Draw(graphics);
+
+            widget.TextAlign = Align.Center;
+            widget.Draw(graphics);
+
+            widget.TextAlign = Align.Right;
+            widget.Draw(graphics);
+
+            Assert.Equal("#Hello World########", canvas.Map[0]);
+            Assert.Equal("#####Hello World####", canvas.Map[1]);
+            Assert.Equal("########Hello World#", canvas.Map[2]);
+            Assert.Equal(3, canvas.Rows);
+        }
+
+        [Fact]
+        public void StopMessageTest()
+        {
+            var canvas = new Canvas(20, 10, '#');
+            var graphics = new Graphics(canvas);
+
+            var widget = new StopMessage("demo");
+            widget.Text = "Hello World";
+
+            widget.Draw(graphics);
+
+            widget.TextAlign = Align.Center;
+            widget.Draw(graphics);
+
+            widget.TextAlign = Align.Right;
+            widget.Draw(graphics);
+
+            Assert.Equal("Hello World#########", canvas.Map[0]);
+            Assert.Equal("#####Hello World####", canvas.Map[1]);
+            Assert.Equal("#########Hello World", canvas.Map[2]);
+            Assert.Equal(3, canvas.Rows);
+        }
+
+        [Fact]
+        public void MarqueeTest()
+        {
+            var canvas = new Canvas(20, 10, '#');
+            var graphics = new Graphics(canvas);
+
+            var widget = new Marquee("demo");
+            widget.Items = new string[] {
+                "Four",
+                "Five",
+                "Nine"
+            };
+
+            widget.Draw(graphics);
+
+            Assert.Equal("┌──────────────────┐", canvas.Map[0]);
+            Assert.Equal("│Four              │", canvas.Map[1]);
+            Assert.Equal("│Five              │", canvas.Map[2]);
+            Assert.Equal("│Nine              │", canvas.Map[3]);
+            Assert.Equal("└──────────────────┘", canvas.Map[4]);
+            Assert.Equal(5, canvas.Rows);
+
+            canvas = new Canvas(20, 10, '#');
+            graphics = new Graphics(canvas);
+
+            widget.TextAlign = Align.Center;
+            widget.Draw(graphics);
+
+            Assert.Equal("┌──────────────────┐", canvas.Map[0]);
+            Assert.Equal("│       Four       │", canvas.Map[1]);
+            Assert.Equal("│       Five       │", canvas.Map[2]);
+            Assert.Equal("│       Nine       │", canvas.Map[3]);
+            Assert.Equal("└──────────────────┘", canvas.Map[4]);
+            Assert.Equal(5, canvas.Rows);
+
+            canvas = new Canvas(20, 10, '#');
+            graphics = new Graphics(canvas);
+
+            widget.TextAlign = Align.Right;
+            widget.Draw(graphics);
+
+            Assert.Equal("┌──────────────────┐", canvas.Map[0]);
+            Assert.Equal("│              Four│", canvas.Map[1]);
+            Assert.Equal("│              Five│", canvas.Map[2]);
+            Assert.Equal("│              Nine│", canvas.Map[3]);
+            Assert.Equal("└──────────────────┘", canvas.Map[4]);
+            Assert.Equal(5, canvas.Rows);
+        }
+
+        [Fact]
+        public void BulletListTest()
+        {
+            var canvas = new Canvas(20, 10, '#');
+            var graphics = new Graphics(canvas);
+
+            var widget = new BulletList("demo");
+            widget.Items = new string[] {
+                "Four",
+                "Five",
+                "Nine"
+            };
+
+            widget.Draw(graphics);
+
+            Assert.Equal("■ Four##############", canvas.Map[0]);
+            Assert.Equal("■ Five##############", canvas.Map[1]);
+            Assert.Equal("■ Nine##############", canvas.Map[2]);
+            Assert.Equal(3, canvas.Rows);
+
+            canvas = new Canvas(20, 10, '#');
+            graphics = new Graphics(canvas);
+
+            widget.TextAlign = Align.Center;
+            widget.Draw(graphics);
+
+            Assert.Equal("#######■ Four#######", canvas.Map[0]);
+            Assert.Equal("#######■ Five#######", canvas.Map[1]);
+            Assert.Equal("#######■ Nine#######", canvas.Map[2]);
+            Assert.Equal(3, canvas.Rows);
+
+            canvas = new Canvas(20, 10, '#');
+            graphics = new Graphics(canvas);
+
+            widget.TextAlign = Align.Right;
+            widget.Draw(graphics);
+
+            Assert.Equal("##############■ Four", canvas.Map[0]);
+            Assert.Equal("##############■ Five", canvas.Map[1]);
+            Assert.Equal("##############■ Nine", canvas.Map[2]);
+            Assert.Equal(3, canvas.Rows);
+        }
+
+        [Fact]
+        public void NumberListTest()
+        {
+            var canvas = new Canvas(20, 10, '#');
+            var graphics = new Graphics(canvas);
+
+            var widget = new NumberList("demo");
+            widget.Items = new string[] {
+                "One",
+                "Two",
+                "Six"
+            };
+
+            widget.Draw(graphics);
+
+            Assert.Equal("1. One##############", canvas.Map[0]);
+            Assert.Equal("2. Two##############", canvas.Map[1]);
+            Assert.Equal("3. Six##############", canvas.Map[2]);
+            Assert.Equal(3, canvas.Rows);
+
+            canvas = new Canvas(20, 10, '#');
+            graphics = new Graphics(canvas);
+
+            widget.TextAlign = Align.Center;
+            widget.Draw(graphics);
+
+            Assert.Equal("#######1. One#######", canvas.Map[0]);
+            Assert.Equal("#######2. Two#######", canvas.Map[1]);
+            Assert.Equal("#######3. Six#######", canvas.Map[2]);
+            Assert.Equal(3, canvas.Rows);
+
+            canvas = new Canvas(20, 10, '#');
+            graphics = new Graphics(canvas);
+
+            widget.TextAlign = Align.Right;
+            widget.Draw(graphics);
+
+            Assert.Equal("##############1. One", canvas.Map[0]);
+            Assert.Equal("##############2. Two", canvas.Map[1]);
+            Assert.Equal("##############3. Six", canvas.Map[2]);
+            Assert.Equal(3, canvas.Rows);
         }
     }
 }
